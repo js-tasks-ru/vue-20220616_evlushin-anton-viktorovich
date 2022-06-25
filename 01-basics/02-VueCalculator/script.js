66f8a0affa5e1be164f0264c8496847f19d79799
Евлушин Антон <aevlushin@atsaero.ru>
2022-06-26 01:32:38 +0300

    Реализован простой калькулятор на Vue

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,50 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+
+const vm = createApp({
+  data() {
+    return {
+      operator: '',
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+  },
+
+  watch: {
+    x() {
+      this.calc();
+    },
+
+    y() {
+      this.calc();
+    },
+
+    operator() {
+      this.calc();
+    },
+  },
+
+  methods: {
+    calc() {
+      switch (this.operator) {
+        case 'sum':
+          this.z = this.x + this.y;
+          break;
+        case 'subtract':
+          this.z = this.x - this.y;
+          break;
+        case 'multiply':
+          this.z = this.x * this.y;
+          break;
+        case 'divide':
+          this.z = this.x / this.y;
+          break;
+        default:
+          this.z = 0;
+          break;
+      }
+    },
+  },
+}).mount('#app');
 
 // Создайте Vue приложение
